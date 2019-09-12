4fb57bc8a274ffa2ed76108a630b9092f01de002
Christian George Echevarria-Reina <christian.george.echevarria@gmail.com>
2019-09-11 21:39:22 -0400

    Add a default zero input array to robustify

--- scripts/bird.js
+++ scripts/bird.js
@@ -22,10 +22,11 @@ let Bird = function(brain) {
   // This is the key function now that decides
   // if it should jump or not jump!
   this.act = function(pipes) {
+    let inputs = [0, 0, 0, 0, 0, 0, 0, 0]
     if(Array.isArray(pipes) && pipes.length) {
       const closest = pipes[0] // closest pipe is always first
       const next = pipes[1] ? pipes[1] : { x: 0, top: 0, bottom: 0 }
-      let inputs = []
+
       inputs[0] = map(closest.x, this.x, 450, 0, 1) // x position of closest pipe
       inputs[1] = map(closest.top, 0, 512, 0, 1) // top of closest pipe opening
       inputs[2] = map(closest.bottom, 0, 512, 0, 1) // bottom of closest pipe opening
@@ -34,10 +35,10 @@ let Bird = function(brain) {
       inputs[5] = map(closest.bottom, 0, 512, 0, 1) // bottom of closest pipe opening
       inputs[6] = map(this.y, 0, 512, 0, 1) // bird's y position
       inputs[7] = map(this.velocity, -5, 5, 0, 1) // bird's y velocity
-
-      let action = this.brain.activate(inputs) // Get the outputs from the network
-      if (action[1] > action[0]) this.jump()
     }
+
+    let action = this.brain.activate(inputs) // Get the outputs from the network
+    if (action[1] > action[0]) this.jump()
   }
 
   this.jump = () => this.velocity += this.lift
